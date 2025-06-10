58f5dabf4b5d63178507d8282d662acf96049121
codeboyzhou <imzhouchen@gmail.com>
2025-06-11 00:19:23 +0800

    refactor(declarative): startServer() method is recommended

--- mcp-server-chat2mysql/chat2mysql-declarative-sdk-example/src/main/java/com/github/mcp/server/chat2mysql/McpSseServer.java
+++ mcp-server-chat2mysql/chat2mysql-declarative-sdk-example/src/main/java/com/github/mcp/server/chat2mysql/McpSseServer.java
@@ -1,8 +1,6 @@
 package com.github.mcp.server.chat2mysql;
 
 import com.github.codeboyzhou.mcp.declarative.McpServers;
-import com.github.codeboyzhou.mcp.declarative.server.McpSseServerInfo;
-import com.github.mcp.server.chat2mysql.util.ServerInfo;
 
 /**
  * Java server implementing Model Context Protocol (MCP) for chat2mysql.
@@ -16,11 +14,7 @@ public class McpSseServer {
      * Main entry point for the HTTP SSE MCP server.
      */
     public static void main(String[] args) {
-        McpSseServerInfo serverInfo = McpSseServerInfo.builder()
-            .name(ServerInfo.SERVER_NAME)
-            .version(ServerInfo.SERVER_VERSION)
-            .build();
-        McpServers.run(McpSseServer.class, args).startSyncSseServer(serverInfo);
+        McpServers.run(McpSseServer.class, args).startServer();
     }
 
 }
--- mcp-server-filesystem/filesystem-declarative-sdk-example/src/main/java/com/github/mcp/examples/server/filesystem/McpSseServer.java
+++ mcp-server-filesystem/filesystem-declarative-sdk-example/src/main/java/com/github/mcp/examples/server/filesystem/McpSseServer.java
@@ -1,8 +1,6 @@
 package com.github.mcp.examples.server.filesystem;
 
 import com.github.codeboyzhou.mcp.declarative.McpServers;
-import com.github.codeboyzhou.mcp.declarative.server.McpSseServerInfo;
-import com.github.mcp.examples.server.filesystem.util.ServerInfo;
 
 /**
  * Java server implementing Model Context Protocol (MCP) for filesystem operations.
@@ -15,11 +13,7 @@ public class McpSseServer {
      * Main entry point for the HTTP SSE MCP server.
      */
     public static void main(String[] args) {
-        McpSseServerInfo serverInfo = McpSseServerInfo.builder()
-            .name(ServerInfo.SERVER_NAME)
-            .version(ServerInfo.SERVER_VERSION)
-            .build();
-        McpServers.run(McpSseServer.class, args).startSyncSseServer(serverInfo);
+        McpServers.run(McpSseServer.class, args).startServer();
     }
 
 }
